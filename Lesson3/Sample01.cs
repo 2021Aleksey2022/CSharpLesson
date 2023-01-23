dfd96c962ccf67f336554d96dcab12dcba516d09
Aleksey <walrus2022@mail.ru>
2023-01-23 18:45:02 +1000

    Третий урок по си шарпу

---
+++ Lesson3/Sample01.cs
@@ -0,0 +1,71 @@
+﻿using HomeWork2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3
+{
+    internal class Sample01 : DzSwitch
+    {
+
+        /// <summary>
+        /// Модификаторы out, ref и метод TryParse.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Class1.OperatorSwitch();
+            /*модификатор ref передаёт значение по ссылке, тобишь значение a, изменится на значение х
+             * str - ссылочный тип, он содержит не значение , а ссылку на объект
+             * TryParse - возвращает истину или ложь, проверяет на ввод данных пользователем
+             * Модификатор out используется для возврата более одного значения из метода. 
+             * Отличие out от ref состоит в том что передаваемую переменную не обязательно инициализировать
+             *  Также метод объявленный с модификатором out обязан присвоить значение соответствующей переменной, 
+             *  до своего завершения и передачи управления вызывающей функции.
+             * 
+             * 
+             */
+            int a = 10;
+            int b = -5;
+            int c = 1;
+            string str = "Hello!";
+
+            int a1;
+            string str2;
+            Sample("ddh", out a1, out str2);
+            DoProcess(ref a, b, c, str);
+            Console.Write("Введите целое число: ");
+            int inputNumber = 1;
+            if(int.TryParse(Console.ReadLine(), out inputNumber))
+            {
+                Console.Write($"Вы ввели число: {inputNumber}\t");
+                if (inputNumber < 10)
+                    Console.WriteLine("Вы ввели число меньше 10");
+                else
+                {
+                    Console.WriteLine("Вы ввели число больше чем 10");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели некорректное число");
+            }
+            Console.ReadLine();
+        }
+        static void DoProcess(ref int x, int y, int z, string mystr)
+        {
+             x = 100;
+             y = 100;
+             z = 100;
+
+        }
+        static bool Sample(string s, out int a, out string str)
+        {
+            a = 10;
+            str = "Hello!";
+            return true;
+        }
+    }
+}
---
+++ Lesson3/Sample02.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilis;
+
+namespace Lesson3
+{
+    /*Структура (struct) в C# — это пользовательский тип данных, 
+     * который используется наряду с классами и может содержать какие-либо данные и методы. 
+     * Структурами также являются такие типы данных как int, double и т.д. 
+     * Основное отличие структуры (struct) от класса (class) заключается в том, что структура — это тип значений, 
+     * а класс — это ссылочный тип.    
+     */
+    //Структура описывает коплексное число
+    struct Complex
+    {
+        //Действительная часть комплексного числа
+        public double re;
+        //Мнимая часть комплексного числа
+        public double im;
+
+
+        /// <summary>
+        /// Сложение комплексных чисел
+        /// </summary>
+        /// <param name="x">Комплексное число</param>
+        /// <returns>Резльтат сложения комплексных чисел</returns>
+        public Complex Plus(Complex x)
+        {
+            Complex complexres;
+            complexres.re = re + x.re;
+            complexres.im = im + x.im;
+            return complexres;
+        }
+        public static Complex Plus(Complex a, Complex b)
+        {
+            Complex complexres;
+            complexres.re = a.re + b.re;
+            complexres.im = a.im + b.im;
+            return complexres;
+        }
+
+        //переопределяем в методе ToString наши поля комплексного числа
+        public override string ToString()
+        {
+            return $"{re} + {im}i";
+        }
+    }
+    internal class Sample02
+    {
+        static void Main(string[] args)
+        {
+            Outputhelpers.PrintInfo(3, "Щербаков Алескй Александрович");
+            int a = 1;
+            char c = 'c';
+            string s = "Hello!";
+            bool b = true;
+            Complex complex1;
+            complex1.re = 5;
+            complex1.im = -2;
+
+            Complex complex2;
+            complex2.re = 4;
+            complex2.im = 3;
+            Complex complex3 = complex1.Plus(complex2);
+
+            Complex complex4 = Complex.Plus(complex1, complex2);
+
+            Console.WriteLine(a);
+            Console.WriteLine(c);
+            Console.WriteLine(b);
+            Console.WriteLine($"Результат сложения комплексных чмсел: {complex1} и {complex2} = {complex3}");
+            Console.ReadLine();
+        }
+        
+    }
+}
